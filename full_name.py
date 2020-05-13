06f3ab6a4016e45c8c76c0d587e8c3704ec25c80
Ville <villejestila@gmail.com>
2020-05-13 14:06:20 +0300

    fullname

---
+++ full_name.py
@@ -0,0 +1,21 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("python")
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favourite_language = 'Python '
+print(favourite_language)
+print(favourite_language.rstrip())
+
+favourite_language2 = ' python '
+print(favourite_language2.rstrip())
+print(favourite_language2.lstrip())
+print(favourite_language2.strip())
