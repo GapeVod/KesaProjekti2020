24c8524f280b25891d150763b3e715f7412ea4f9
Ville <villejestila@gmail.com>
2020-06-25 09:41:29 +0300

    chapter 13 80%

--- 13-1star/star.py
+++ 13-1star/star.py
@@ -1,17 +1,16 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Alien(Sprite):
+class Star(Sprite):
 	"""A class to represent a single alien in the fleet."""
 
 	def __init__(self, ai_game):
 		"""Initialize the alien and set its starting position."""
 		super().__init__()
 		self.screen = ai_game.screen
-		self.settings = ai_game.settings
 
 		#Load the alien image and set its rect attribute.
-		self.image = pygame.image.load('images/alien.bmp')
+		self.image = pygame.image.load('images/star.bmp')
 		self.rect = self.image.get_rect()
 
 		#Start each new alien near the top left of the screen.
@@ -20,15 +19,3 @@ class Alien(Sprite):
 
 		#Store the alien's exact horizontal position.
 		self.x = float(self.rect.x)
-
-	def check_edges(self):
-		"""Return True if alein is at edge of screen."""
-		screen_rect = self.screen.get_rect()
-		if self.rect.right >= screen_rect.right or self.rect.left <= 0:
-			return True
-
-	def update(self):
-		"""Move the alien to the right."""
-		self.x += (self.settings.alien_speed *
-			self.settings.fleet_direction)
-		self.rect.x = self.x
--- 13-3raindrops/raindrops.py
+++ 13-3raindrops/raindrops.py
@@ -90,19 +90,6 @@ class AlienInvasion:
     		if bullet.rect.bottom <= 0:
     			self.bullets.remove(bullet)
 
-    	self._check_bullet_alien_collisions()
-
-    def _check_bullet_alien_collisions(self):
-    	"""Respond to bullet-alien collisions."""
-    	#Remove any bullets and alien that have collided.
-    	collisions = pygame.sprite.groupcollide(
-    		self.bullets, self.aliens, True, True)
-
-    	if not self.aliens:
-    		#Destroy existing bulelts and create a new fleet.
-    		self.bullets.empty()
-    		self._create_fleet()
-
     def _create_fleet(self):
     	"""Create the fleet of aliens."""
     	#Create an alien and find the number of aliens in a row.
