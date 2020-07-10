a93cb5576791c1d827103913660839c284673090
Ville <villejestila@gmail.com>
2020-07-10 22:12:25 +0300

    asd

--- Data_Visualization/die_visual.py
+++ Data_Visualization/die_visual.py
@@ -20,12 +20,12 @@ die_2 = Die()
 #Make some rolls, and store results in a list.
 results = []
 for roll_num in range(50_000):
-	result = die_1.roll()  die_2.roll() #+ die_3.roll()
+	result = die_1.roll() * die_2.roll() #+ die_3.roll()
 	results.append(result)
 
 #Analyze the results.
 frequencies = []
-max_result = die_1.num_sides  die_2.num_sides #+ die_3.num_sides
+max_result = die_1.num_sides * die_2.num_sides #+ die_3.num_sides
 for value in range(2, max_result+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
