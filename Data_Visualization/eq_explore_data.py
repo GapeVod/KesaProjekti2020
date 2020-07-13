91e860c85ef3f199d74b59b89a809e4ce0962380
Ville <villejestila@gmail.com>
2020-07-14 01:03:35 +0300

    asd

--- Data_Visualization/eq_explore_data.py
+++ Data_Visualization/eq_explore_data.py
@@ -14,19 +14,10 @@ all_eq_titles = all_eq_data['metadata']['title']
 
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-	#mag = eq_dict['properties']['mag']
-	#lon = eq_dict['geometry']['coordinates'][0]
 	mags.append(eq_dict['properties']['mag'])
 	lons.append(eq_dict['geometry']['coordinates'][0])
 	lats.append(eq_dict['geometry']['coordinates'][1])
 	hover_texts.append(eq_dict['properties']['title'])
-	#lat = eq_dict['geometry']['coordinates'][1]
-	#title = eq_dict['properties']['title']
-	#mags.append(mag)
-	#lons.append(lon)
-	#lats.append(lat)
-	#hover_texts.append(title)
-
 
 #Map the earthquakes.
 data = [{
@@ -46,11 +37,3 @@ my_layout = Layout(title=all_eq_titles)
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
-
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
-
-#readable_file = 'readable_eq_data.json'
-#with open(readable_file, 'w') as f:
-#	json.dump(all_eq_data, f, indent=4)
